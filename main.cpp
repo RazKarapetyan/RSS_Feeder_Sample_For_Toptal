925a06ab93cc1a616e18b6037fbebb47989f9873
Razmik Karapetyan <Razmik.Karapetyan@teamviewer.com>
2021-12-04 04:24:04 +0400

    add app icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "GUI/RSSFeeder.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":icons/rss.png"));
     RSSFeeder w;
     w.show();
     return a.exec();
